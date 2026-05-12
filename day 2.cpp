6335ed09b7a20cedad047b82bd46d852b02abccf
ujjwalmorecpp-art <ujjwal.more.cpp@gmail.com>
2026-05-13 05:13:38 +0530

    Create day 2.cpp

---
+++ day 2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+
+    int a = 5;
+    int b = 9;
+
+    cout<< "This is my day 2 of learning coding language.\n Valu of a is "<<a<<" \n The value of b is "<<b;
+   
+    return 0; 
